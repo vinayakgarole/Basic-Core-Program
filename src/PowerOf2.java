773c37c846e389c63ad64ad83fa43a2a275e6028
vinayakgarole <vinayakgarole10@gmail.com>
2022-08-22 17:22:55 +0530

    added: PowerOf2

--- src/PowerOf2.java
+++ src/PowerOf2.java
@@ -1,20 +1,15 @@
-// input no=5
-// power p=2
-//5*5=25
 import java.util.Scanner;
-class PowerOf2 {
-    public static void main(String[] args) {
-        int n,p,result=1;
-        System.out.println("Enter No. ");
-        Scanner r=new Scanner(System.in);
-        n=r.nextInt();
-        System.out.println("Enter Power ");
-        p=r.nextInt();
 
-        for(int i=1;i<=p;i++)
-        {
-            result=n*result;
+public class PowerOf2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter powers of Two : ");
+        int x = sc.nextInt();
+        int n = 2;
+        int result = 1;
+        for (int i = 1; i <= x; i++) {
+            result = n * result;
         }
-        System.out.println("result "+result);
+        System.out.println("Power = " + result);
     }
 }
