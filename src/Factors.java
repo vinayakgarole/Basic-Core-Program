b1e5199b090c87358e6d23a4d8af76fd71abebcf
vinayakgarole <vinayakgarole10@gmail.com>
2022-08-23 11:33:17 +0530

    added: Factors

--- src/Factors.java
+++ src/Factors.java
@@ -1,21 +1,15 @@
 import java.util.Scanner;
-public class Factors {
+
+class Factors {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number");
-        int n = sc.nextInt();
-
-        System.out.println("Prime Factors of " + n + " are - ");
-
-        int i = 2;
-        while (n > 1) {
+        int n;
+        System.out.println("Enter any Number");
+        Scanner r = new Scanner(System.in);
+        n = r.nextInt();
+        for (int i = 1; i <= n; i++) {
             if (n % i == 0) {
-                System.out.print(i + " ");
-                n = n / i;
-            } else
-                i++;
+                System.out.println(i + " ");
+            }
         }
-        System.out.println(" ");
-        sc.close();
     }
 }
